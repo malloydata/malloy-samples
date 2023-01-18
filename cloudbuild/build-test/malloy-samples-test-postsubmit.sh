65dd0b115fde3d16bc019979c21a4ded9ca93ab6
Will Scullin <willscullin@google.com>
2023-01-18 01:05:26 +0000

    Fix curl, fail on error

--- cloudbuild/build-test/malloy-samples-test-postsubmit.sh
+++ cloudbuild/build-test/malloy-samples-test-postsubmit.sh
@@ -2,6 +2,7 @@
 set -euxo pipefail
 
 nix-shell --quiet --pure --command "$(cat <<NIXCMD
+  set -euxo pipefail
   cd /workspace
   npm ci --silent
   make -C duckdb/imdb
--- default.nix
+++ default.nix
@@ -1,1 +1,1 @@
-with import <nixpkgs> {}; stdenv.mkDerivation { name = "malloy"; buildInputs = [ nodejs-16_x jdk8 google-cloud-sdk duckdb fakeroot]; }
+with import <nixpkgs> {}; stdenv.mkDerivation { name = "malloy"; buildInputs = [ nodejs-16_x jdk8 google-cloud-sdk cacert curl duckdb fakeroot]; }
