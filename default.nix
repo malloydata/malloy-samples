24c1ef51e23a641dc01180c88aa9daa6e5f1d898
Carlin Eng <carlineng@google.com>
2023-09-20 20:18:26 -0700

    Ce/samples fixes (#33)
    Fix samples, bump Malloy version

--- default.nix
+++ default.nix
@@ -1,1 +1,1 @@
-with import <nixpkgs> {}; stdenv.mkDerivation { name = "malloy"; buildInputs = [ nodejs-16_x jdk8 google-cloud-sdk cacert curl duckdb fakeroot]; }
+with import <nixpkgs> {}; stdenv.mkDerivation { name = "malloy"; buildInputs = [ nodejs-18_x jdk8 google-cloud-sdk cacert curl duckdb fakeroot]; }
